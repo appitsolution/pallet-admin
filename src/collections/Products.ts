ec2e6f79ac26c43a6042fed0322b6519b0c0ba32
appitsolution <applicationitsolution@gmail.com>
2023-05-15 21:12:40 +0300

    added first commit

---
+++ src/collections/Products.ts
@@ -0,0 +1,64 @@
+import { CollectionConfig } from "payload/types";
+
+// Example Collection - For reference only, this must be added to payload.config.ts to be used.
+const Products: CollectionConfig = {
+  slug: "products",
+  admin: {
+    useAsTitle: "name",
+  },
+  access: {
+    read: () => true,
+  },
+  fields: [
+    {
+      name: "id",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "name",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "span",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "size",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "weight",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "upload",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "description",
+      type: "textarea",
+      required: true,
+    },
+    {
+      name: "images",
+      type: "array",
+      required: true,
+      fields: [
+        {
+          name: "catalog",
+          type: "upload",
+          relationTo: "media",
+          required: true,
+        },
+      ],
+    },
+  ],
+};
+
+export default Products;
---
+++ src/payload.config.ts
@@ -0,0 +1,20 @@
+import { buildConfig } from "payload/config";
+import path from "path";
+// import Examples from './collections/Examples';
+import Users from "./collections/Users";
+import Products from "./collections/Products";
+import Media from "./collections/Media";
+
+export default buildConfig({
+  serverURL: "http://localhost:3001",
+  admin: {
+    user: Users.slug,
+  },
+  collections: [Users, Products, Media],
+  typescript: {
+    outputFile: path.resolve(__dirname, "payload-types.ts"),
+  },
+  graphQL: {
+    schemaOutputFile: path.resolve(__dirname, "generated-schema.graphql"),
+  },
+});
